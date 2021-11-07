f1e80167c735658aa2a1c6dc64080899ba318e70
aabduvakhobov <aabduvakhobov02@gmail.com>
2021-11-07 15:20:12 +0500

    Created the project on ASP.NET Core MVC and added Cars and Category Models

---
+++ CW1-WebAppUI-8408/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using CW1_WebAppUI_8408.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace CW1_WebAppUI_8408.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            //Hosted web API REST Service base url
+            string Baseurl = "http://localhost:57719/";
+            List<Car> CarInfo = new List<Car>();
+            using (var client = new HttpClient())
+            {
+                //Passing service base url
+                client.BaseAddress = new Uri(Baseurl);
+                client.DefaultRequestHeaders.Clear();
+                //Define request data format
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                 HttpResponseMessage Res = await client.GetAsync("api/Product");
+            if (Res.IsSuccessStatusCode)
+                {
+                    var PrResponse = Res.Content.ReadAsStringAsync().Result;
+                     CarInfo = JsonConvert.DeserializeObject<List<Car>>(PrResponse);
+                }
+                //returning the Product list to view
+                return View(CarInfo);
+            }
+        }
+    }
+}
---
+++ CW1-WebAppUI-8408/Models/ErrorViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CW1_WebAppUI_8408.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
